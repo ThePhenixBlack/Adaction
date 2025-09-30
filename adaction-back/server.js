4edd20e5d9835fd3549d946fc3a570e4395bec2a
matteo <matteobriffazpro@gmail.com>
2025-09-30 11:55:37 +0200

    test

--- adaction-back/server.js
+++ adaction-back/server.js
@@ -15,4 +15,20 @@ const data = { "plate": "Hello World Burger", "description": "Un cheeseburger cl
 res.json(data);
 });
 
-app.listen(3000, () => {  console.log("Serveur lancé sur http://localhost:3000");});
+require("dotenv").config();
+
+const http = require("http");
+const { neon } = require("@neondatabase/serverless");
+
+const sql = neon(process.env.DATABASE_URL);
+
+const requestHandler = async (req, res) => {
+  const result = await sql`SELECT version()`;
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
+};
+
+http.createServer(requestHandler).listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
