55f3cf9b83633931f18f640e46a6f552088daa7c
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-10-01 10:00:16 +0200

    add post road benevoles

--- adaction-back/server.js
+++ adaction-back/server.js
@@ -5,6 +5,7 @@ import { Pool } from 'pg';
 
 dotenv.config()
 const app = express();
+app.use(express.json())
 app.use(cors({ origin: "http://127.0.0.1:5500"}));
 
 const pool = new Pool({
@@ -31,5 +32,24 @@ app.get("/benevoles", async (req, res) => {
     
 })
 
+//
+
+
+app.post("/benevoles", async (req, res) => {
+    try {
+      const query = `
+        INSERT INTO benevoles (firstname, lastname, city, password)
+        VALUES ($1, $2, $3, $4) RETURNING *
+      `;
+      const values = ["nourdine", "ali", "nanterre", "1234"];
+      const result = await pool.query(query, values);
+  
+      res.status(201).json(result.rows[0]); 
+    } catch (error) {
+      console.error("erreur POST /benevoles:", error);
+      res.status(500).json({ error: "Erreur serveur" });
+    }
+  });
+//
 
 app.listen(3000, () => {  console.log("Serveur lancé sur http://localhost:3000");});
