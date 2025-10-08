4481840ff3c0e229b66439166e086d5425cf3d32
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-10-08 14:41:57 +0200

    css

--- adaction-front/scripts/benevole.js
+++ adaction-front/scripts/benevole.js
@@ -5,6 +5,10 @@ if (!id) window.location.href = "login.html"
 
 
 
+
+const sectionProfilCard = document.querySelector(".profile-card")
+console.log(sectionProfilCard)
+
 async function profilVolunteer() {
   const resp = await fetch(`http://localhost:3000/benevoles/${id}`);
   if (!resp.ok) {
@@ -40,6 +44,7 @@ async function afficherTotal() {
   const total = await totalVolunteers(); // on attend le résultat
   const totalDiv = document.getElementById("totalDechets-span");
   totalDiv.textContent = `${total}`;
+  console.log(total)
 }
 afficherTotal()
   const showProfil = (data) => {
@@ -57,7 +62,6 @@ afficherTotal()
 
 
 
-//  Ajouter en bas les fetch back et les élements dynamiques des collectes
 
 const newCollecte = document.getElementById("new-collecte");
 newCollecte.addEventListener("click", () => {
@@ -117,126 +121,6 @@ console.log(megots)
   }
 
 
-
-
-
-
-
-
-
-
-//  async function fetchForm(){
-
-//    try {
-//       const response = await fetch("http://localhost:3000/collectes", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({
-//           city: location,
-//           benevole_id: benevole_id,
-//           name: ,
-//           quantity: quantity
-//         })
-//       });
-//       if (!response.ok) {
-//         throw new Error("Erreur lors de l'envoi des données");
-//       }
-//       const result = await response.json();
-//       console.log("Collecte ajoutée avec succès :", result);
-//     } catch (error) {
-//       console.error("Erreur:", error);
-//     }
-//   }
-
-
- 
-
-
- // Initialisation de la date du jour
-// const dateInput = document.getElementById("date");
-// const today = new Date().toISOString().split("T")[0];
-// dateInput.value = today;
-// dateInput.min = today;
-// dateInput.max = today;
-
-// const validateButton = document.getElementById("validate-button");
-// const trashList = document.getElementById("trash-list");
-// const totalQuantityDiv = document.getElementById("total-quantity");
-
-// validateButton.addEventListener("click", async () => {
-//   trashList.innerHTML = "";
-//   let total = 0;
-
-//   const date = dateInput.value;
-//   const location = document.getElementById("location").value;
-//   const selectedTrash = document.querySelectorAll('input[name="trash-type"]:checked');
-
-//   if (!date || !location || selectedTrash.length === 0) {
-//     alert("Veuillez remplir tous les champs correctement.");
-//     return;
-//   }
-
-//   const trashItems = [];
-//   selectedTrash.forEach(trash => {
-//     const trashName = trash.value;
-//     const quantityInput = document.getElementById(`quantity-${trashName}`);
-//     const quantity = parseInt(quantityInput.value, 10) || 0;
-
-//     if (quantity <= 0) {
-//       alert("Veuillez entrer une quantité valide pour tous les déchets cochés.");
-//       return;
-//     }
-
-//     // Ajouter à la liste affichée
-//     const li = document.createElement("li");
-//     li.textContent = `${trashName}: - ${quantity}`;
-//     li.dataset.name = trashName;
-//     li.dataset.quantity = quantity;
-//     trashList.appendChild(li);
-
-//     total += quantity;
-//     trashItems.push({ name: trashName, quantity });
-//   });
-
-//   totalQuantityDiv.textContent = `Quantité totale: ${total}`;
-
-//   // Réinitialiser les cases cochées et champs quantité
-//   selectedTrash.forEach(trash => trash.checked = false);
-//   selectedTrash.forEach(trash => {
-//     const quantityInput = document.getElementById(`quantity-${trash.value}`);
-//     quantityInput.value = "";
-//   });
-
-  // const benevole_id = id; 
-  // for (const item of trashItems) {
-  //   try {
-  //     const response = await fetch("http://localhost:3000/collectes", {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify({
-  //         city: location,
-  //         date: date,
-  //         benevole_id: benevole_id,
-  //         name: item.name,
-  //         quantity: item.quantity
-  //       })
-  //     });
-  //     if (!response.ok) {
-  //       throw new Error("Erreur lors de l'envoi des données");
-  //     }
-  //     const result = await response.json();
-  //     console.log("Collecte ajoutée avec succès :", result);
-  //   } catch (error) {
-  //     console.error("Erreur:", error);
-  //   }
-  // }
-
-            
-
-
-
-
-
 const collectesId = async (benevole_id) => {
     const response = await fetch(`http://localhost:3000/collectes/${benevole_id}`);
     const collectes = await response.json();
@@ -287,6 +171,7 @@ document.getElementById("btn-modification").addEventListener("click", () => {
 
   const form = document.createElement("form");
   form.id = "formUpdateVolunteers";
+  sectionProfilCard.appendChild(form)
 
   const inputFirstname = document.createElement("input");
   inputFirstname.type = "text";
@@ -335,7 +220,7 @@ form.appendChild(inputLastname);
     profilVolunteer(); // recharge le profil mis à jour
   });
 
-  document.querySelector("header").appendChild(form);
+
 });
 async function fetchModified(volunteer) {
   try {
